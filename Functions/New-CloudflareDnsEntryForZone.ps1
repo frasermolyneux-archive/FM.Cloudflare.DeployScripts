e96889fc85f1e80feb9d409abb423f20fe094e8a
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2019-06-30 15:52:17 +0100

    Adding function to create dns entry in zone

---
+++ Functions/New-CloudflareDnsEntryForZone.ps1
@@ -0,0 +1,43 @@
+function New-CloudflareDnsEntryForZone {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)] [String] $AuthKey,
+        [Parameter(Mandatory = $true)] [String] $AuthEmail,
+        [Parameter(Mandatory = $true)] [String] $ZoneId,
+        [Parameter(Mandatory = $true)] [Hashtable] $DnsEntry
+    )
+    
+    begin {
+        Write-Debug "Begin creating new DNS entry in $ZoneId"
+    }
+    
+    process {
+
+        $headers = @{
+            "Content-Type" = "application/json"
+            "X-Auth-Key" = $AuthKey
+            "X-Auth-Email" = $AuthEmail
+        }
+
+        $body = ConvertTo-Json $DnsEntry
+
+        Write-Debug "POST: https://api.cloudflare.com/client/v4/zones/$ZoneId/dns_records"
+        Write-Debug $body
+
+        $response = Invoke-RestMethod -Uri "https://api.cloudflare.com/client/v4/zones/$ZoneId/dns_records" -Method "POST" -Body $body -Headers $headers
+
+        Write-Debug $response
+
+        if ($response.success -eq $true) {
+            $response.result
+        }
+        else {
+            Write-Error "Failed to create new DNS entry"
+            Write-CloudflareErrorMessage -Response $response
+        }
+    }
+    
+    end {
+        Write-Debug "End creating new DNS entry in $ZoneId"
+    }
+}
