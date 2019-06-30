d02e975ff910f4dee5fa3dc6c089c090f91be927
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2019-06-30 15:50:26 +0100

    Creating basic project structure

---
+++ Execute-Tests.ps1
@@ -0,0 +1,20 @@
+$DebugPreference = 'SilentlyContinue'
+$VerbosePreference = 'SilentlyContinue'
+$InformationPreference = 'SilentlyContinue'
+
+Import-Module Pester
+Set-Location $PSScriptRoot
+
+$moduleRoot = "$PSScriptRoot"
+
+Get-ChildItem -Path "$PSScriptRoot\Internal" -Filter "*.ps1" | ForEach-Object {
+    . $_.FullName
+}
+
+Get-ChildItem -Path "$PSScriptRoot\Functions" -Filter "*.ps1" | ForEach-Object {
+    . $_.FullName
+}
+
+Get-ChildItem -Path "$PSScriptRoot\Tests" -Filter "*.Tests.ps1" | ForEach-Object {
+    . $_.FullName -ModuleRoot $moduleRoot
+}
